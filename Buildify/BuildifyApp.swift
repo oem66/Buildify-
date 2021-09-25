87aa6cfe833c004dff529b883af470d2e3f07867
oem66 <omer.6g@gmail.com>
2021-09-25 11:53:30 +0200

    setup Firebase

--- Buildify/BuildifyApp.swift
+++ Buildify/BuildifyApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct BuildifyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             LandingView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        debugPrint("Setting up Firebase")
+        FirebaseApp.configure()
+        return true
     }
 }
